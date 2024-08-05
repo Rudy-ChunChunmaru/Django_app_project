5afa8ed29ae1fe57a139f9f3a6ae93d2e1611750
Rudy <95736349+Rudy-ChunChunmaru@users.noreply.github.com>
2024-08-05 10:11:51 +0700

    update backend

--- django-react-app/backend/app_users/views.py
+++ django-react-app/backend/app_users/views.py
@@ -1,4 +1,4 @@
-from rest_framework import generics
+from rest_framework import generics,status
 from django.contrib.auth.models import User
 from rest_framework.response import Response
 from rest_framework.permissions import AllowAny
@@ -15,3 +15,12 @@ class GetUserView(generics.ListAPIView):
     queryset = User.objects.all()
     serializer_class = UserSerializer
     permission_classes =[AllowAny]
+
+    def get(self,request,id=None,*args, **kwargs):
+        if(id):
+            user_query = User.objects.filter(id__icontains = id)
+        else:
+            user_query = User.objects.all()
+        result =  UserSerializer(user_query, many=True)
+        return Response(data=result.data , status = status.HTTP_200_OK)
+

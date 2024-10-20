2814c85e626dc51a272d6560c7cc3ddb52bdd584
Rudy <95736349+Rudy-ChunChunmaru@users.noreply.github.com>
2024-10-20 22:57:04 +0700

    update all

--- django-react-app/backend/web/serializer.py
+++ django-react-app/backend/web/serializer.py
@@ -9,6 +9,7 @@ class UserSerializer(serializers.ModelSerializer):
         fields = ["id","username","first_name","last_name","password"]
         extra_kwargs = {"password" : {"write_only" : True}}
 
+
     def create(self,validated_data):
         user = User.objects.create_user(**validated_data)
         return user
--- django-react-app/backend/web/view/auth.py
+++ django-react-app/backend/web/view/auth.py
@@ -17,32 +17,32 @@ from rest_framework.response import Response
 from rest_framework.permissions import AllowAny,IsAuthenticated
 
 from django.contrib.auth.models import User
-from ..serializer import UserSerializer,UserMenuPermissionSerializer,UserGroupSerializer
+from ..serializer import UserSerializer,UserMenuPermissionSerializer
 from ..models import user_menu_permission
 
 class AuthUserLogin(APIView):
     permission_classes = [AllowAny]
 
     def post(self, request):
-        username = request.data.get('username')
-        password = request.data.get('password')
-        if(username and password):
-            user = authenticate(request,username=username,password=password)
-            if user is not None:
-                querysetUser = User.objects.filter(username=user).first()
-                resultUser = UserSerializer(querysetUser)
-
+        reqUsername = request.data.get('username')
+        reqPassword = request.data.get('password')
+        if(reqUsername and reqPassword):
+            strusername = authenticate(request,username=reqUsername,password=reqPassword)
+            if strusername is not None:
+                querysetUser = User.objects.filter(username=strusername).first()
+                # print(querysetUser.values().first())
+                resultUser = UserSerializer(querysetUser,many=False)
+                print(resultUser.data)
                 # resultGroup = UserGroupSerializer(querysetUser)
                 # print(resultGroup.data)
-
                 # print(resultUser.data['id'])
-                # querysetMenu = user_menu_permission.objects.filter(intUser_id_id = resultUser.data['id'])
-                # resultMenu = UserMenuPermissionSerializer(querysetMenu)
-                # print(resultMenu.data)
+                querysetMenu = user_menu_permission.objects.filter(intUser_id_id = resultUser.data['id']).first()
+                resultMenu = UserMenuPermissionSerializer(querysetMenu,many=False)
+                print(resultMenu.data)
                 return Response(data={
-                    'user': resultUser.data,
+                    # 'user': querysetUser.values,
                     # 'menu': resultMenu.data,
-                    'token' : get_tokens_for_user(user)
+                    'token' : get_tokens_for_user(strusername)
                     }, status=status.HTTP_200_OK)
             else:
                 return Response(data={'detail' : 'username and password invalid !!!'}, status=status.HTTP_401_UNAUTHORIZED)

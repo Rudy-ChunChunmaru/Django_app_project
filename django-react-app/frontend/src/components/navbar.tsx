3a17d7ddd6fc1ef42a6c4e2677619c7cb6a28793
Rudy <95736349+Rudy-ChunChunmaru@users.noreply.github.com>
2024-11-04 20:07:07 +0700

    django app bar style update

--- django-react-app/frontend/src/components/navbar.tsx
+++ django-react-app/frontend/src/components/navbar.tsx
@@ -4,6 +4,8 @@ import { useNavigate } from "react-router-dom";
 import NavbarMenu from "./navbar-menu";
 import { jwtDecode } from "jwt-decode";
 
+import vite from "../asset/photo/vite.svg";
+
 const Navbar = () => {
   const [loginStatus, setLoginStatus] = useState<boolean>(false);
 
@@ -79,9 +81,14 @@ const Navbar = () => {
 
   return (
     <div className="mx-auto flex w-[100%] flex-col border-b-2">
-      <div className="flex w-full justify-between py-1">
-        <div className="my-auto p-1">
-          <h1>APP NAME</h1>
+      <div className="flex w-full justify-between px-3 py-1">
+        <div className="my-auto flex h-fit w-fit gap-4 p-1">
+          <img
+            className="mx-auto my-auto h-[60%] w-[60%]"
+            src={vite}
+            alt={vite}
+          />
+          <div className="w-[100%]">APP NAME</div>
         </div>
         <div className="flex min-w-[10%] max-w-fit flex-col px-2">
           {loginStatus && <User />}

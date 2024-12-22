5157e5bf1c314538f76b369488aff58f3326fa36
rudymuinfo <rudymuinfo@gmail.com>
2024-12-22 05:53:40 +0000

    litel update on group

--- django-react-app/frontend/src/pages/apps/setting/group/index.tsx
+++ django-react-app/frontend/src/pages/apps/setting/group/index.tsx
@@ -4,17 +4,19 @@ type Props = {};
 
 const Group = (props: Props) => {
   const colom:tableColomType[] = [
-    {lable:"Username",colom:"username",type:'string',input:true},
+    {lable:"Group",colom:"name",type:'string',input:true},
     {lable:"First Name",colom:"first_name",type:'string',input:true},
-    {lable:"Last Name",colom:"last_name",type:'string',input:true},
-    {lable:"Email",colom:"email",type:'string',input:true},
-    {lable:"Super User",colom:"is_superuser",type:'boolean',input:true},
-    {lable:"Staff",colom:"is_staff",type:'boolean',input:true},
-    {lable:"Active",colom:"is_active",type:'boolean',input:true},
-    {lable:"Action",colom:"url",type:'action',input:false}
   ]
 
-  return <div>Group</div>;
+  return <div className="w-full text-normal">
+  <div className="w-full border-b-2
+    border-gray-300
+    dark:border-gray-500
+  ">List Group</div>
+    <div className="w-full">
+      <TableGrid link={"api/web/setting/group/"} coloms={colom} />
+    </div>
+</div>;
 };
 
 export default Group;
